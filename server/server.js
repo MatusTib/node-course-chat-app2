eb1700c10b235ba1c2dba4b11d26ea6de6dd5b2c
Florence <stamandf@gmail.com>
2018-10-23 14:47:29 -0400

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,27 +1,38 @@
-require('./config/config');
+// require('./config/config');
 const path = require('path');
-
-const publicPath = path.join(__dirname, '/../public');
-
+const http = require('http');
 const express = require('express');
-const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
 
-// app.use(bodyParser.json());
-console.log(path);
-console.log(publicPath + '/index.html');
-
-let app = express();
-
+// const bodyParser = require('body-parser');
+const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+let userCount =0;
 
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html');
+io.on('connection', (socket) => {
+  console.log(('New user connected.'));
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected.');
+  });
+
 });
 
-app.listen(port, () => {  //To add: if(!module.parent) {} This will resolve the Error: listen EADDRINUSE :::3000
+
+server.listen(port, () => {  //To add: if(!module.parent) {} This will resolve the Error: listen EADDRINUSE :::3000
   console.log(`Started up on port ${port}`);
 });
 
-module.exports = {app}
+// app.get('/', (req, res) => {
+//   res.sendFile('index.html');
+// });
+// app.use(bodyParser.json());
+// console.log(path);
+// console.log(publicPath + '/index.html');
+//
+// module.exports = {app}
