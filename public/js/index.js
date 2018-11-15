8c2a7f590c02b91cc16b604cb36d9195b2703dac
Florence <stamandf@gmail.com>
2018-11-15 15:47:46 -0500

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -20,19 +20,19 @@ socket.on('newLocationMessage', function (message) {
   var li = $('<li></li>');
   var a = $('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}`);
+  li.text(`${message.from}: `);
   a.attr('href', message.url);
   li.append(a);
   $('#messages').append(li);
 });
   $('#message-form').on('submit', function (e) {
     e.preventDefault();
-
+    var messageTextbox = $('#inputmsg');
     socket.emit('createMessage', {
       from: 'User',
-      text: $('#inputmsg').val()
+      text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('')
     })
   })
 
@@ -41,12 +41,16 @@ socket.on('newLocationMessage', function (message) {
     if(!navigator.geolocation) {
       return alert('Geolocation not supported by your browser.');
     }
+
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(function(position) {
+      locationButton.removeAttr('disabled').text('Send location');
       socket.emit('createLocationMessage', {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
     },function () {
+          locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
   });
