fd3236ca2c49f3ebe29651ff9e5b89f1f40325ad
Florence <stamandf@gmail.com>
2018-12-20 14:03:12 -0500

    Added icons to buttons.  Changed border radius to make them round.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,7 +1,5 @@
 let socket = io();
 
-
-
 function scrollToBottom () {
   // Selectors
   let messages = $('#messages');
@@ -86,16 +84,18 @@ socket.on('newLocationMessage', function (message) {
     if(!navigator.geolocation) {
       return alert('Geolocation not supported by your browser.');
     }
-
-    locationButton.attr('disabled', 'disabled').text('Sending location...');
+    locationButton.attr('disabled', 'disabled');
+    // locationButton.attr('disabled', 'disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(function(position) {
-      locationButton.removeAttr('disabled').text('Send location');
+      locationButton.removeAttr('disabled');
+      // locationButton.removeAttr('disabled').text('Send location');
       socket.emit('createLocationMessage', {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
     },function () {
-          locationButton.removeAttr('disabled').text('Send location');
+          locationButton.removeAttr('disabled');
+          // locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
   });
@@ -115,14 +115,6 @@ socket.on('newLocationMessage', function (message) {
         img: imageUrl,
         createdAt: formattedTime
       });
-      // console.log('In newImageMessage, img ByteLength is:', message.img.byteLength);
-      // console.log('In newImageMessage, img is:',message.img);
-      // console.log('In newImageMessage, Uint8Array ba is:',ba);
-      //
-      // console.log('urlCreator is:',urlCreator);
-      //
-      // let reader = new FileReader();
-
       $('#messages').append(html);
       scrollToBottom();
       imageUrl = urlCreator.revokeObjectURL( blob );
@@ -131,33 +123,28 @@ socket.on('newLocationMessage', function (message) {
     //Upload an image
     let imageSelect = $('#image-select');
     let imageFile = $('#image-file');
-
     imageSelect.on('click', function () { //when user clicks on the image triger the input file
-        console.log('User clicked!');  //---------TRACE
-        imageSelect.attr('disabled', 'disabled'); //---------TRACE
+        // console.log('User clicked!');  //---------TRACE
+        imageSelect.attr('disabled', 'disabled');
         imageFile.click();
         let count = 0; //PATCH: to fix problem of duplicate images when user selects an image one after another
         console.log('imageFile:', imageFile);
 
         imageFile.change (function () {
-
-          console.log('IMAGE FILE CHANGE!', imageFile.change);
-          console.log('COUNT=', count);
-          console.log('imageFile Val=', imageFile.val());
-          // let fileName = imageFile.val().split('\\')[imageFile.val().split('\\').length-1];
-          let file = document.querySelector('input[type=file]').files[0];
-
-          if(file && count < 1) {
-
-            console.log('Files:', document.querySelector('input[type=file]').files);
-            let fileName = file.name;//---------TRACE
-            let fileType = file.type;//---------TRACE
-            let fileSize = file.size;//---------TRACE
-
-            console.log('FILE = ', file);//---------TRACE
-            console.log('FILE NAME = ', fileName);//---------TRACE
-            console.log('FILE TYPE = ', fileType);//---------TRACE
-            console.log('FILE SIZE = ', fileSize);//---------TRACE
+            // console.log('IMAGE FILE CHANGE!', imageFile.change);//---------TRACE
+            // console.log('COUNT=', count);//---------TRACE
+            // console.log('imageFile Val=', imageFile.val());//---------TRACE
+            let file = document.querySelector('input[type=file]').files[0];
+            if(file && count < 1) {
+                // console.log('Files:', document.querySelector('input[type=file]').files); //---------TRACE
+                // let fileName = file.name;//---------TRACE
+                // let fileType = file.type;//---------TRACE
+                // let fileSize = file.size;//---------TRACE
+            //
+            // console.log('FILE = ', file);//---------TRACE
+            // console.log('FILE NAME = ', fileName);//---------TRACE
+            // console.log('FILE TYPE = ', fileType);//---------TRACE
+            // console.log('FILE SIZE = ', fileSize);//---------TRACE
             // let reader = new FileReader();
 
             // if (file) {
@@ -168,22 +155,19 @@ socket.on('newLocationMessage', function (message) {
             // console.log('Selected fileName:', fileName);//---------TRACE
             // console.log('Selected file:', file);//---------TRACE
             //
-            let messageTextbox = $('#inputmsg');
+                let messageTextbox = $('#inputmsg');
+                socket.emit('createImageMessage', {
+                  img: file,
+                }, function () {
+                        // console.log('in File read createImageMessage');
+                        // console.log('in File read createImageMessage, FILE 1= ',file);
+                        messageTextbox.val('');
+                        // file = null;
+                        // console.log('in File read createImageMessage, FILE 2= ',file);
+                });
 
-            socket.emit('createImageMessage', {
-              img: file,
-            }, function () {
-                    // console.log('in File read createImageMessage');
-                    // console.log('in File read createImageMessage, FILE 1= ',file);
-                    messageTextbox.val('');
-                    file = null;
-                    // console.log('in File read createImageMessage, FILE 2= ',file);
-            });
-
-            count = count + 1;
-          }
+                count = count + 1; //To prevent duplicate image messages. Count number of loops.
+              }
         });
         imageSelect.removeAttr('disabled');
-
-
     });
